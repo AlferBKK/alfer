597eea448d0b1bc3f3a5aaacdde41735ed5d0423
Wittawat Kitipatthavorn <earthwittawat2005@gmail.com>
2023-09-16 02:36:04 +0700

    error fix

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "768340f1523c761f"; }
+/******/ 	__webpack_require__.h = function() { return "d0ccb114584877c6"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
