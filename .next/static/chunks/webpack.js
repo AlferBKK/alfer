c898340df1e5a0d59f9efaeae9ebd0f4e83303b5
Earth <63118195+EarthWittawat@users.noreply.github.com>
2023-09-16 13:09:22 +0700

    error fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "81d76f267e54a53a"; }
+/******/ 		__webpack_require__.h = function() { return "e9d48d5fa26c9a1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/spline/spline.tsx
+++
@@ -1,9 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function SplineComponent() {
-  return (
-    <div>
-      <Spline scene="https://prod.spline.design/YN0xrxyM2HxNbpHd/scene.splinecode" />
-    </div>
-  );
-}
