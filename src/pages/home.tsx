5953785400311db8cccbc1d615f18599b7d8a639
wit03 <witjarukit@gmail.com>
2023-09-16 21:42:09 +0700

    feat: mpabox and .gitignores

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,33 @@
-import {Sidebar} from "../components/sidebar/sidebar";
+import { Sidebar } from "../components/sidebar/sidebar";
 import SplineComponent from "../components/spline/spline";
+import Map from 'react-map-gl';
 import React from "react";
+
 export default function Home() {
   return (
     <main>
-    <div className="flex flex-row">
-    <Sidebar/>
-    <div className="flex w-[100%] bg-white">
-        <div className="p-7">
-      <h1 className="text-2xl text-black font-semibold">Dashboard</h1>
-      <div>Demo Spline</div>
-      <SplineComponent/>
-    </div>
-    </div>
-    </div>
+      <div className="flex flex-row">
+        <Sidebar />
+        <div className="flex w-[100%] bg-white">
+          <div className="p-7">
+            <h1 className="text-2xl text-black font-semibold">Dashboard</h1>
+            <div>Demo Spline</div>
+            <SplineComponent />
+            <Map
+              mapLib={import('mapbox-gl')}
+              initialViewState={{
+                longitude: -100,
+                latitude: 40,
+                zoom: 3.5
+              }}
+              style={{ width: 600, height: 400 }}
+              mapStyle="mapbox://styles/mapbox/streets-v9"
+              mapboxAccessToken="pk.eyJ1Ijoid2l0MDMiLCJhIjoiY2xtbTNwaXloMGZ5bTJqdGNsc2Q4OW15NCJ9.Ugu_Thdk8_F4oDlOPs6_7Q"
+            />;
+
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
